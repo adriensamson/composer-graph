00ce135e412e3c87b4ec9311aa1d1b8cd3d8c153
Adrien Samson <adrien@kyklydse.com>
2013-06-01 16:13:54 +0200

    added command and app

---
+++ src/Kyklydse/ComposerGraph/GraphCommand.php
@@ -0,0 +1,31 @@
+<?php
+namespace Kyklydse\ComposerGraph;
+
+
+use Composer\Factory;
+use Composer\IO\ConsoleIO;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\Input;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\Output;
+
+class GraphCommand extends Command
+{
+    public function __construct()
+    {
+        parent::__construct('graph');
+    }
+
+    protected function configure()
+    {
+        $this->addArgument('file', InputArgument::OPTIONAL, 'Composer file to parse', 'composer.json');
+    }
+
+    public function execute(Input $input, Output $output)
+    {
+        $file = $input->getArgument('file');
+        $io = new ConsoleIO($input, $output, $this->getHelperSet());
+        $composer = Factory::create($io, $file);
+        $output->writeln($composer->getPackage()->getName());
+    }
+}
